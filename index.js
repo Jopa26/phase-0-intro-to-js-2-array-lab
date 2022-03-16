63b00f5726d2e63489a743d131578a3eec0e7a61
Luis <jqluis2011@gmail.com>
2022-03-15 20:59:00 -0700

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+
+function destructivelyAppendCat(Michi){
+
+cats.push(Michi);
+
+}
+
+function  destructivelyPrependCat(Michi){
+
+    cats.unshift(Michi);
+}
+
+function destructivelyRemoveLastCat(Michi){
+
+    cats.pop(Michi)
+}
+
+function destructivelyRemoveFirstCat(Michi){
+
+    cats.shift(Michi)
+}
+
+function appendCat(Michi){
+    
+    const catstwo = [...cats,Michi];
+
+    return catstwo;
+    
+}
+
+function  prependCat(Michi){
+
+    const catsone = [Michi,...cats];
+
+    return catsone;
+}
+
+function removeLastCat(){
+
+    const removes = cats.slice(0,-1);
+
+    return removes;
+}
+
+function removeFirstCat(){
+
+    const removesfirst = cats.slice(1);
+
+    return removesfirst;
+}
